4356b42bbac4b1f2f8cd515bcfa295fabdd56026
Alabi Temitope <t.banji@rocketmail.com>
2022-12-11 21:15:23 +0100

    updated with latest key

--- public/assets/js/remoteData.js
+++ public/assets/js/remoteData.js
@@ -17,7 +17,7 @@ function getRepoCount() {
     method: 'POST',
     body: JSON.stringify(queryData),
     headers: {
-      'Authorization': `Bearer  er`,
+      'Authorization': `Bearer  ghp`,
       'Content-Type': 'Application/json'
     }
   }).then(res => {
@@ -30,7 +30,10 @@ function getRepoCount() {
     .then(async (parsedRes) => {
       console.log('parsedRes ff', parsedRes);
       const data = await parsedRes;
-      getInfo(data);
+      if (data) {
+        console.warn('datamm', data);
+        getInfo(data);
+      }
     })
 }
 
@@ -93,7 +96,7 @@ function getInfo(repoCount) {
     method: 'POST',
     body: JSON.stringify(queryData),
     headers: {
-      'Authorization': `Bearer  er`,
+      'Authorization': `Bearer  ghp`,
       'Content-Type': 'Application/json'
     }
   }).then(res => {
@@ -104,7 +107,7 @@ function getInfo(repoCount) {
     } else { throw new Error('network error'); }
   })
     .then(async (parsedRes) => {
-      getRepoList(parsedRes, repoCount);
+      await getRepoList(parsedRes, repoCount);
     })
 }
 
@@ -120,10 +123,12 @@ function getRepoList(arr, count) {
   let profileBio = "";
   let repoPubCount = "";
   let repoTotalCount = "";
-  let profileImg = "";
+  let profileImg ="";
   profileImg = arr.data.user.avatarUrl;
   profileAvatar = arr.data.user.avatarUrl;
 
+  console.warn('arr.data.viewer', arr.data.viewer);
+  // profileImage = arr.data.viewer.avatarUrl;
   profileBio = arr.data.viewer.bio;
   profileName = arr.data.viewer.name;
   profileUsername = arr.data.viewer.login;
@@ -156,17 +161,17 @@ function getRepoList(arr, count) {
                 </span>
             
                 <span class="mr3 ml3" style='display:  ${repoArr[j]['node']['object'] && repoArr[j]['node']['object']['repository']
-        && `${repoArr[j]['node']['object']['repository']['stargazerCount']}` > 0 ? 'inline-block' : 'none'}; 
+        && `${repoArr[j]?.['node']?.['object']?.['repository']?.['stargazerCount']}` > 0 ? 'inline-block' : 'none'}; 
              ' ><svg class="octicon octicon-star mr4"
                       viewBox="0 0 16 16" version="1.1" width="12" height="12" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
-             <span class="fz12 fcolor">${repoArr[j]['node']['object']['repository']['stargazerCount']}</span>
+             <span class="fz12 fcolor">${repoArr[j]?.['node']?.['object']?.['repository']?.['stargazerCount']}</span>
                 </span>
 
                 <span class="mr10 ml10" style='display:  ${repoArr[j]['node']['object'] && repoArr[j]['node']['object']['repository']
         && `${repoArr[j]['node']['object']['repository']['refs']}`
         && `${repoArr[j]['node']['object']['repository']['refs']['totalCount']}` > 0 ? 'inline-block' : 'none'}; 
              ' ><span ><svg class="octicon octicon-git-branch text-gray" height="12" viewBox="0 0 16 16" version="1.1" width="12" aria-hidden="true"><path fill-rule="evenodd" d="M11.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122V6A2.5 2.5 0 0110 8.5H6a1 1 0 00-1 1v1.128a2.251 2.251 0 11-1.5 0V5.372a2.25 2.25 0 111.5 0v1.836A2.492 2.492 0 016 7h4a1 1 0 001-1v-.628A2.25 2.25 0 019.5 3.25zM4.25 12a.75.75 0 100 1.5.75.75 0 000-1.5zM3.5 3.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0z"></path></svg></span>
-             <span class="fz12 fcolor">${repoArr[j]['node']['object']['repository']['refs']['totalCount']}</span>
+             <span class="fz12 fcolor">${repoArr[j]?.['node']?.['object']?.['repository']?.['refs']?.['totalCount']}</span>
                 </span>
              Updated on
              <time datetime="2008-02-14 20:00" style="white-space: nowrap!important;"
