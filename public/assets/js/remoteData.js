7d60160eb125cb7e35255c9b49bfa438fe5250dc
Alabi Temitope <t.banji@rocketmail.com>
2022-12-15 17:17:32 +0100

    updated Readme

--- public/assets/js/remoteData.js
+++ public/assets/js/remoteData.js
@@ -17,7 +17,7 @@ function getRepoCount() {
     method: 'POST',
     body: JSON.stringify(queryData),
     headers: {
-      'Authorization': `Bearer  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`,
+      'Authorization': `Bearer  gmi`,
       'Content-Type': 'Application/json'
     }
   }).then(res => {
@@ -96,7 +96,7 @@ function getInfo(repoCount) {
     method: 'POST',
     body: JSON.stringify(queryData),
     headers: {
-      'Authorization': `Bearer  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`,
+      'Authorization': `Bearer  gmi`,
       'Content-Type': 'Application/json'
     }
   }).then(res => {
